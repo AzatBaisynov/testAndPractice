b721ff47ead04a53d44c8d510bf5d48fc60f876a
Azat <azat.baisynov@gmail.com>
2020-01-09 23:08:21 +0600

    first commit

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("проверяем тернарный оператор:\n" +
+                "чтобы закончить а или b должны быть = 0\n" +
+                "введите число а и число b:");
+
+        while (1 > 0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            String result = (a > b) ? "a > b" : "a < b";
+            System.out.println(result);
+            if (a == 0 || b == 0){
+                break;
+            }
+        }
+
+    }
+}
